9fcb15b887ea6ba8bcdb5fc09852abe1bea9574b
Aspirai <1766789972@qq.com>
2024-10-27 12:01:59 +0800

    docs: 更新文档说明

--- README.md
+++ README.md
@@ -12,12 +12,59 @@
 ![image](https://github.com/Aspirai/BarrageAnalysis/blob/master/img/7.png)
 ![image](https://github.com/Aspirai/BarrageAnalysis/blob/master/img/8.png)
 
-
 # 快速使用
 
-front_end文件夹下运行```yarn dev```或者```npm run dev```;
+## 前端启动
 
-back_end文件夹下运行```python manage.py runserver```;
+1. 进入front_end文件夹下运行```npm install```
+
+   ![image-20241027112830470](C:\Users\Administrator\AppData\Roaming\Typora\typora-user-images\image-20241027112830470.png)
+
+2. 若出现上面提示,运行```npm audit fix --force```修复
+
+   ![image-20241027112955417](C:\Users\Administrator\AppData\Roaming\Typora\typora-user-images\image-20241027112955417.png)
+
+3. 运行```npm run dev```启动前端项目
+
+   ![image-20241027113201493](C:\Users\Administrator\AppData\Roaming\Typora\typora-user-images\image-20241027113201493.png)
+
+4. 浏览器进入```http://localhost:5173/```
+
+   ![image](https://github.com/Aspirai/BarrageAnalysis/blob/master/img/1.png)
+
+## 后端启动
+
+### 数据库配置
+
+1. 安装数据库```MySQL```,[链接](https://blog.csdn.net/m0_71422677/article/details/136007088)
+
+2. 安装数据库管理```Navicat Premium 16```,[链接](https://www.cnblogs.com/kkdaj/p/16260681.htm)
+
+3. 打开Navicat ,新建链接,后续不用可以删除
+
+   ![image-20241027114729645](C:\Users\Administrator\AppData\Roaming\Typora\typora-user-images\image-20241027114729645.png)
+
+4. 新建数据库```test```
+
+   ![image-20241027114523040](C:\Users\Administrator\AppData\Roaming\Typora\typora-user-images\image-20241027114523040.png)
+
+   ![image-20241027114825178](C:\Users\Administrator\AppData\Roaming\Typora\typora-user-images\image-20241027114825178.png)
+
+5. 引入表结构(在上面)
+
+   ![image-20241027115030361](C:\Users\Administrator\AppData\Roaming\Typora\typora-user-images\image-20241027115030361.png)
+
+   ![image-20241027115109845](C:\Users\Administrator\AppData\Roaming\Typora\typora-user-images\image-20241027115109845.png)
+
+   ![image-20241027115133115](C:\Users\Administrator\AppData\Roaming\Typora\typora-user-images\image-20241027115133115.png)
+
+   ## 启动!
+
+   1. 进入```back_end```,运行```python manage.py runserver```
+
+      ![image-20241027115553142](C:\Users\Administrator\AppData\Roaming\Typora\typora-user-images\image-20241027115553142.png)
+
+   启动完成
 
 # 文档说明
 
--- front_end/src/components/login/Login2.vue
+++ front_end/src/components/login/Login2.vue
@@ -1,6 +1,7 @@
 <template>
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
     <div class="background"></div>
+
     <body>
         <img src="../../assets/img/22.gif" class="pic" ref="pic" alt="pic" style="position: absolute;">
         <!-- <header class="header">
@@ -10,8 +11,8 @@
                 <a href="../../assets/html/poetry.html">诗词</a>
             </nav>
             <form action="" class="search-bar"> -->
-                <!-- placeholder属性是用来设置表单元素的占位符文本的。-->
-                <!-- <input type="text" placeholder="搜索...">
+        <!-- placeholder属性是用来设置表单元素的占位符文本的。-->
+        <!-- <input type="text" placeholder="搜索...">
                 <button><i class='bx bx-search'></i></button>
             </form>
         </header> -->
@@ -20,7 +21,8 @@
             <div class="item">
                 <h2 class="logo"><i class='bx bxl-vimeo'>Love</i></h2>
                 <div class="text-item">
-                    <h2>弹幕情感分析系统</h2><br><h3><span>开始使用</span></h3>
+                    <h2>弹幕情感分析系统</h2><br>
+                    <h3><span>开始使用</span></h3>
                     <p>永远不要沉溺在安逸里得过且过，能给你遮风挡雨的，同样能让你不见天日。只有让自己更加强大，才能真正地撑起一片天。</p>
                     <div class="social-icon">
                         <a href="#"><i class="bx bxl-facebook"></i></a>
@@ -57,9 +59,9 @@
                 </div>
                 <div class="form-box register">
                     <form action="">
-    
+
                         <h2>注册</h2>
-    
+
                         <div class="input-box">
                             <span class="icon"><i class="bx bxs-user"></i></span>
                             <input type="text" v-model="register_from.uname" required>
@@ -93,176 +95,186 @@
 </template>
 
 <script>
-import { ElMessage } from 'element-plus'
-import Axios from 'axios';
-export default{
-    name:'Login2',
-    data() {
-        return {
-            Form: {
-                email:"",
-                password: "",
+    import { ElMessage } from 'element-plus'
+    import Axios from 'axios';
+    export default {
+        name: 'Login2',
+        data() {
+            return {
+                Form: {
+                    email: "",
+                    password: "",
+                },
+                register_from: {
+                    uname: "",
+                    email: "",
+                    password: ""
+                },
+                admin: {},
+            }
+        },
+        created() {
+            // let email = document.getElementById('email').value
+            // console.log(email)
+            this.start();
+        },
+        methods: {
+            start() {
+                var url = "http://127.0.0.1:8000/api/admin/";
+                Axios.get(url).then(response => { this.admin = response.data; })
+                console.log("链接成功")
+                console.log(this.admin)
             },
-            register_from:{
-                uname:"",
-                email:"",
-                password:""
+            login() {
+                event.preventDefault();
+                let num = this.admin.length
+                for (let i = 0; i < num; i++) {
+                    console.log(this.admin[i].user_email)
+                    if (this.Form.email.length == 0 && this.Form.password.length == 0) {
+                        ElMessage.error('请填写邮箱或者密码')
+                        break;
+                    } else if (this.Form.email == this.admin[i].user_email && this.Form.password == this.admin[i].pass_word) {
+                        this.$router.push('/home')
+                        console.log("success")
+                        ElMessage({
+                            message: '登录成功！',
+                            type: 'success',
+                        })
+                        break;
+                    } else if (i == num - 1) {
+                        ElMessage({
+                            message: '邮箱或者密码不正确',
+                            type: 'warning',
+                        })
+                    }
+                }
             },
-            admin:{},
-        }
-    },
-    created(){
-        // let email = document.getElementById('email').value
-        // console.log(email)
-        this.start();
-  },
-  methods: {
-    start(){
-        var url = "http://127.0.0.1:8000/api/admin/";
-        Axios.get(url).then(response => {this.admin = response.data;})
-        console.log("链接成功")
-        console.log(this.admin)
-    },
-    login(){
-        event.preventDefault();
-        let num = this.admin.length
-        for(let i=0;i<num;i++){
-            console.log(this.admin[i].user_email)
-            if (this.Form.email.length == 0 && this.Form.password.length == 0) {
-                ElMessage.error('请填写邮箱或者密码')
-                break;
-            } else if (this.Form.email == this.admin[i].user_email && this.Form.password == this.admin[i].pass_word) {
-                this.$router.push('/home')
-                console.log("success")
+            register() {
+                var url = "http://127.0.0.1:8000/api/admin/";
+                Axios.post(url, { 'user_name': this.register_from.uname, 'pass_word': this.register_from.password, 'user_email': this.register_from.email })
                 ElMessage({
-                    message: '登录成功！',
+                    message: '注册成功！',
                     type: 'success',
                 })
-                break;
-            } else if(i == num-1){
-                ElMessage({
-                    message: '邮箱或者密码不正确',
-                    type: 'warning',
-                })
             }
-        }
-    },
-    register(){
-        var url = "http://127.0.0.1:8000/api/admin/";
-        Axios.post(url,{'user_name':this.register_from.uname,'pass_word':this.register_from.password,'user_email':this.register_from.email})
-        ElMessage({
-            message: '注册成功！',
-            type: 'success',
-        })
-    }
-  },
-  mounted(){
-    const pic = this.$refs.pic;
-    document.addEventListener('mousemove', (e) => {
-        // 计算图片应该移动到的位置
-        //   console.log("原",e.clientX,e.clientY)
-        const x = e.clientX + 10;
-        const y = e.clientY - 820;
-        //   console.log(x,y)
+        },
+        mounted() {
+            const pic = this.$refs.pic;
+            document.addEventListener('mousemove', (e) => {
+                // 计算图片应该移动到的位置
+                //   console.log("原",e.clientX,e.clientY)
+                const x = e.clientX + 10;
+                const y = e.clientY - 820;
+                //   console.log(x,y)
 
-        // 更新图片的位置
-        pic.style.left = x + 'px';
-        pic.style.top = y + 'px';
-    });
-    const loginsec = document.querySelector('.login-section')
-    const loginlink = document.querySelector('.login-link')
-    const registerlink = document.querySelector('.register-link')
-    registerlink.addEventListener('click',()=>{
-        loginsec.classList.add('active')
-    })
-    loginlink.addEventListener('click',()=>{
-        loginsec.classList.remove('active')
-    })
-  }
-}
+                // 更新图片的位置
+                pic.style.left = x + 'px';
+                pic.style.top = y + 'px';
+            });
+            const loginsec = document.querySelector('.login-section')
+            const loginlink = document.querySelector('.login-link')
+            const registerlink = document.querySelector('.register-link')
+            registerlink.addEventListener('click', () => {
+                loginsec.classList.add('active')
+            })
+            loginlink.addEventListener('click', () => {
+                loginsec.classList.remove('active')
+            })
+        }
+    }
 </script>
 <style>
-*{
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-    font-family: "Simsun",sans-serif;
-}
-body{
-    height: 100vh;
-    width: 100%;
-    background-color: black;
-    position: fixed;
-}
-.background{
-    background: url(../../assets/img/background2.jpg) no-repeat;
-    background-position: center;
-    background-size: over;
-    height: 100vh;
-    width: 100%;
-    /* 用于为元素添加一个视觉效果。它可以用于实现模糊、色彩变换、亮度调整等效果。 */
-    filter: blur(12px);
-}
-.container{
-    position: absolute;
-    left: 50%;
-    top: -50%;
-    transform: translate(-50%,-50%);
-    width: 75%;
-    height: 550px;
-    margin-top: 20px;
-    background: url(../../assets/img/background2.jpg) no-repeat;
-    background-position: center;
-    background-size: over;
-    border-radius: 20px;
-    overflow: hidden;
-}
-.item{
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 58%;
-    height: 100%;
-    color: #fff;
-    background-color: transparent;
-    padding: 80px;
-    display: flex;
-    justify-content: space-between;
-    flex-direction: column;
-}
-.item .logo{
-    color: #fff;
-    font-size: 30px;
-}
-.text-item h2{
-    font-size: 36px;
-    line-height: 1.2;
-    margin: -12px auto;
-}
-.text-item p{
-    font-size: 16px;
-    margin: 20px 0;
-    line-height: 1.5;
-}
-.social-icon a i{
-    color: #fff;
-    font-size: 24px;
-    margin-left: 10px;
-    cursor: pointer;
-    transition: .5s ease;
-}
-.social-icon a:hover i{
-    transform: scale(1.2);
-}
-.container .login-section{
-    position: absolute;
-    top: 0;
-    right: 0;
-    /* calc计算元素的宽度为父元素宽度减去58% */
-    width: calc(100% - 58%);
-    height: 100%;
-    color: #fff;
-    /* 是CSS中的一个属性，用于为元素的背景添加一个视觉效果。它可以用于实现模糊、色彩变换、亮度调整等效果。以下是backdrop-filter的常见取值：
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+        font-family: "Simsun", sans-serif;
+    }
+
+    body {
+        height: 100vh;
+        width: 100%;
+        background-color: black;
+        position: fixed;
+    }
+
+    .background {
+        background: url(../../assets/img/background2.jpg) no-repeat;
+        background-position: center;
+        background-size: cover;
+        height: 100vh;
+        width: 100%;
+        /* 用于为元素添加一个视觉效果。它可以用于实现模糊、色彩变换、亮度调整等效果。 */
+        filter: blur(12px);
+    }
+
+    .container {
+        position: absolute;
+        left: 50%;
+        top: -50%;
+        transform: translate(-50%, -50%);
+        width: 75%;
+        height: 550px;
+        margin-top: 20px;
+        background: url(../../assets/img/background2.jpg) no-repeat;
+        background-position: center;
+        background-size: cover;
+        border-radius: 20px;
+        overflow: hidden;
+    }
+
+    .item {
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 58%;
+        height: 100%;
+        color: #fff;
+        background-color: transparent;
+        padding: 80px;
+        display: flex;
+        justify-content: space-between;
+        flex-direction: column;
+    }
+
+    .item .logo {
+        color: #fff;
+        font-size: 30px;
+    }
+
+    .text-item h2 {
+        font-size: 36px;
+        line-height: 1.2;
+        margin: -12px auto;
+    }
+
+    .text-item p {
+        font-size: 16px;
+        margin: 20px 0;
+        line-height: 1.5;
+    }
+
+    .social-icon a i {
+        color: #fff;
+        font-size: 24px;
+        margin-left: 10px;
+        cursor: pointer;
+        transition: .5s ease;
+    }
+
+    .social-icon a:hover i {
+        transform: scale(1.2);
+    }
+
+    .container .login-section {
+        position: absolute;
+        top: 0;
+        right: 0;
+        /* calc计算元素的宽度为父元素宽度减去58% */
+        width: calc(100% - 58%);
+        height: 100%;
+        color: #fff;
+        /* 是CSS中的一个属性，用于为元素的背景添加一个视觉效果。它可以用于实现模糊、色彩变换、亮度调整等效果。以下是backdrop-filter的常见取值：
     blur(radius)：为元素的背景添加高斯模糊效果，其中radius表示模糊半径。
     brightness(value)：调整元素的背景亮度，其中value表示亮度值，可以是百分比或小数。
     contrast(value)：调整元素的背景对比度，其中value表示对比度值，可以是百分比或小数。
@@ -272,138 +284,156 @@ body{
     opacity(value)：调整元素的背景透明度，其中value表示透明度值，可以是百分比或小数。
     saturate(value)：调整元素的背景饱和度，其中value表示饱和度值，可以是百分比或小数。
     sepia(value)：将元素的背景转换为棕褐色图像，其中value表示棕褐色值，可以是百分比或小数。 */
-    backdrop-filter: blur(10px);
-}
-.login-section .form-box{
-    position: absolute;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    width: 100%;
-    height: 100%;
-}
+        backdrop-filter: blur(10px);
+    }
 
-.login-section .form-box.register{
-    transform: translateX(530px);
-    transition: transform .6s ease;
-    transition-delay: 0s;
-}
-.login-section.active .form-box.register{
-    transform: translateX(0px);
-    transition-delay: .7s;
-}
+    .login-section .form-box {
+        position: absolute;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        width: 100%;
+        height: 100%;
+    }
 
-.login-section .form-box.login{
-    transform: translateX(0px);
-    transition: transform .6s ease;
-    transition-delay: 0.7s;
-}
-.login-section.active .form-box.login{
-    transform: translateX(530px);
-    transition-delay: 0s;
-}
+    .login-section .form-box.register {
+        transform: translateX(530px);
+        transition: transform .6s ease;
+        transition-delay: 0s;
+    }
 
-.login-section .form-box h2{
-    text-align: center;
-    font-size: 25px;
-}
-.form-box .input-box{
-    width: 340px;
-    height: 50px;
-    border-bottom: 2px solid #fff;
-    margin: 30px 0;
-    position: relative;
-}
-.input-box input{
-    width: 100%;
-    height: 100%;
-    background: transparent;
-    border: none;
-    outline: none;
-    font-size: 16px;
-    padding-right: 28px;  
-    color: #fff;
- }
- .input-box label{
-    position: absolute;
-    top: 31%;
-    left: 0;
-    transform: translateT(-50%);
-    font-size: 16px;
-    font-weight: 600px;
-    pointer-events: none;
-    transition: .5s ease;
- }
- .input-box .icon{
-    position: absolute;
-    top: 13px;
-    right: 0;
-    font-size: 19px;
- }
- /* 当用户聚焦在<input>元素上时，与其相邻的<label>元素的颜色将变为红色。 */
-.input-box input:focus~ label,
-/* 当用户在<input>元素中填写了有效值时，与其相邻的<label>元素的颜色将变为绿色。 */
-.input-box input:valid~ label{
-    top: -15px;
-    color: aquamarine;
-}
-.remember-password{
-    font-size: 14px;
-    font-weight: 500;
-    margin: -15px 0 15px;
-    display: flex;
-    justify-content: space-between;
-}
-.remember-password label input{
-    accent-color: #fff;
-    margin-right: 3px;
-}
-.remember-password a{
-    color: #fff;
-    text-decoration: none;
-}
-.remember-password a:hover{
-    text-decoration: underline;
-}
-.btn{
-    background: #fff;
-    width: 100%;
-    height: 45px;
-    outline: none;
-    border: none;
-    border-radius: 4px;
-    cursor: pointer;
-    background: #0362d8;
-    font-size: 16px;
-    color: #fff;
-    box-shadow: rgba(0, 0, 0, 0.4);
+    .login-section.active .form-box.register {
+        transform: translateX(0px);
+        transition-delay: .7s;
+    }
 
-}
-.create-accout{
-    font-size: 14.5px;
-    text-align: center;
-    margin: 25px;
+    .login-section .form-box.login {
+        transform: translateX(0px);
+        transition: transform .6s ease;
+        transition-delay: 0.7s;
+    }
 
-}
-.create-accout p a{
-    color: #fff;
-    font-weight: 600px;
-    text-decoration: none;
-}
-.create-accout p a:hover{
-    text-decoration: underline;
-}
-.foot p{
-    color: #fff;
-    position: absolute;
-    bottom: 0;
-    font-size: 15px;
-    left: 47%;
-}
-.pic{
-    position: absolute;
-    width: 60px;
-    height: 60px;
-    z-index: 111;
-}
+    .login-section.active .form-box.login {
+        transform: translateX(530px);
+        transition-delay: 0s;
+    }
+
+    .login-section .form-box h2 {
+        text-align: center;
+        font-size: 25px;
+    }
+
+    .form-box .input-box {
+        width: 340px;
+        height: 50px;
+        border-bottom: 2px solid #fff;
+        margin: 30px 0;
+        position: relative;
+    }
+
+    .input-box input {
+        width: 100%;
+        height: 100%;
+        background: transparent;
+        border: none;
+        outline: none;
+        font-size: 16px;
+        padding-right: 28px;
+        color: #fff;
+    }
+
+    .input-box label {
+        position: absolute;
+        top: 31%;
+        left: 0;
+        transform: translateT(-50%);
+        font-size: 16px;
+        font-weight: 600px;
+        pointer-events: none;
+        transition: .5s ease;
+    }
+
+    .input-box .icon {
+        position: absolute;
+        top: 13px;
+        right: 0;
+        font-size: 19px;
+    }
+
+    /* 当用户聚焦在<input>元素上时，与其相邻的<label>元素的颜色将变为红色。 */
+    .input-box input:focus~label,
+    /* 当用户在<input>元素中填写了有效值时，与其相邻的<label>元素的颜色将变为绿色。 */
+    .input-box input:valid~label {
+        top: -15px;
+        color: aquamarine;
+    }
+
+    .remember-password {
+        font-size: 14px;
+        font-weight: 500;
+        margin: -15px 0 15px;
+        display: flex;
+        justify-content: space-between;
+    }
+
+    .remember-password label input {
+        accent-color: #fff;
+        margin-right: 3px;
+    }
+
+    .remember-password a {
+        color: #fff;
+        text-decoration: none;
+    }
+
+    .remember-password a:hover {
+        text-decoration: underline;
+    }
+
+    .btn {
+        background: #fff;
+        width: 100%;
+        height: 45px;
+        outline: none;
+        border: none;
+        border-radius: 4px;
+        cursor: pointer;
+        background: #0362d8;
+        font-size: 16px;
+        color: #fff;
+        box-shadow: rgba(0, 0, 0, 0.4);
+
+    }
+
+    .create-accout {
+        font-size: 14.5px;
+        text-align: center;
+        margin: 25px;
+
+    }
+
+    .create-accout p a {
+        color: #fff;
+        font-weight: 600px;
+        text-decoration: none;
+    }
+
+    .create-accout p a:hover {
+        text-decoration: underline;
+    }
+
+    .foot p {
+        color: #fff;
+        position: absolute;
+        bottom: 0;
+        font-size: 15px;
+        left: 47%;
+    }
+
+    .pic {
+        position: absolute;
+        width: 60px;
+        height: 60px;
+        z-index: 111;
+    }
 </style>
